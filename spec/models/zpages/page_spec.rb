3b6a2042982a314d8aba35e407e28fcca1b0b344
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-01-23 17:58:41 -0200

    Refactored Zpages::Page and Zpages::Par specs

--- spec/models/zpages/page_spec.rb
+++ spec/models/zpages/page_spec.rb
@@ -1,63 +1,61 @@
 require 'spec_helper'
 
-module Zpages
-  describe Page do
-    it { should_not allow_mass_assignment_of(:options) }
-    it { should allow_mass_assignment_of(:slug) }
-    it { should allow_mass_assignment_of(:template_name) }
-    it { should allow_mass_assignment_of(:title) }
+describe Zpages::Page do
+  it { should_not allow_mass_assignment_of(:options) }
+  it { should allow_mass_assignment_of(:slug) }
+  it { should allow_mass_assignment_of(:template_name) }
+  it { should allow_mass_assignment_of(:title) }
+  it { should validate_presence_of(:slug) }
+  it { should validate_uniqueness_of(:slug).case_insensitive }
+  it { should validate_presence_of(:title) }
+  it { should have_many(:parts) }
+  it { should accept_nested_attributes_for(:parts) }
+end
 
-    it { should validate_presence_of(:slug) }
-    it { should validate_uniqueness_of(:slug).case_insensitive }
-    it { should validate_presence_of(:title) }
 
-    it { should have_many(:parts) }
-    it { should accept_nested_attributes_for(:parts) }
+describe Zpages::Page, '.factory' do
+  let(:page_config) do
+    Zpages::Config::Page.new(:page_name, {
+      attributes: {
+        title: { type: :string},
+        body:  { type: :html},
+      }
+    })
+  end
 
-    describe '.factory' do
-      let(:page_config) do
-        Config::Page.new(:page_name, {
-          attributes: {
-            title: { type: :string},
-            body:  { type: :html},
-          }
-        })
-      end
+  subject { described_class.factory(page_config) }
+  its(:template_name) { should eq('page_name') }
+  its('parts.count') { should eq(2) }
 
-      subject { described_class.factory(page_config) }
+  it "has a part named title" do
+    subject.parts.map(&:name).should include('title')
+  end
 
-      its(:template_name) { should eq('page_name') }
-      its('parts.count') { should eq(2) }
+  it "has a part named body" do
+    subject.parts.map(&:name).should include('body')
+  end
 
-      it "has a part named title" do
-        subject.parts.map(&:name).should include('title')
-      end
+  it "has parts with the attribute options saved on it" do
+    subject.parts.find_by_name(:title).options.should eq(
+      HashWithIndifferentAccess.new({ type: :string})
+    )
+  end
+end #.factory
 
-      it "has a part named body" do
-        subject.parts.map(&:name).should include('body')
-      end
 
-      it "has parts with the attribute options saved on it" do
-        subject.parts.find_by_name(:title).options.should eq(
-          HashWithIndifferentAccess.new({ type: :string})
-        )
-      end
-    end #.factory
+describe Zpages::Page, "#part" do
+  it "gets part by name" do
+    page = Zpages::Page.factory(Zpages::Config::Page.new(:page_name, {
+      attributes: {
+        title: { type: :string},
+        body:  { type: :html},
+      }
+    }))
 
-    describe "#part" do
-      it "gets part by name" do
-        page = Page.factory(Config::Page.new(:page_name, {
-          attributes: {
-            title: { type: :string},
-            body:  { type: :html},
-          }
-        }))
-
-        page.part('title').should be_a Part
-        page.part('title').name.should == 'title'
-        page.part(:body).should be_a Part
-        page.part(:body).name.should == 'body'
-      end
-    end
+    page.part('title').should be_a Zpages::Part
+    page.part('title').name.should == 'title'
+    page.part(:body).should be_a Zpages::Part
+    page.part(:body).name.should == 'body'
   end
 end
+
--- spec/models/zpages/part_spec.rb
+++ spec/models/zpages/part_spec.rb
@@ -1,29 +1,27 @@
 require 'spec_helper'
 
-module Zpages
-  describe Part do
-    it { should allow_mass_assignment_of(:file) }
-    it { should allow_mass_assignment_of(:value) }
-    it { should allow_mass_assignment_of(:name) }
+describe Zpages::Part do
+  it { should allow_mass_assignment_of(:file) }
+  it { should allow_mass_assignment_of(:value) }
+  it { should allow_mass_assignment_of(:name) }
 
-    it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:page_id) }
-    it { should validate_presence_of(:page) }
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:page_id) }
+  it { should validate_presence_of(:page) }
+end
 
-    describe "#options" do
-      let(:hash) { HashWithIndifferentAccess.new(:option_1 => 'value 1', 'option_2' => 'value 2') }
-      let(:part_id) { FactoryGirl.create(:zpages_part, options: hash).id }
 
-      it "is serialized" do
-        # force reload
-        part = Part.where('id > 0').where(id: part_id).first
-        part.options.should be_a HashWithIndifferentAccess
-        part.options.should have_key('option_1')
-        part.options.should have_key(:option_2)
-        part.options[:option_1].should eq('value 1')
-        part.options[:option_2].should eq('value 2')
-      end
-    end
+describe Zpages::Part, "#options" do
+  let(:hash) { HashWithIndifferentAccess.new(:option_1 => 'value 1', 'option_2' => 'value 2') }
+  let(:part_id) { FactoryGirl.create(:zpages_part, options: hash).id }
 
+  it "is a serialized param" do
+    # force reload
+    part = Zpages::Part.where('id > 0').where(id: part_id).first
+    part.options.should be_a HashWithIndifferentAccess
+    part.options.should have_key('option_1')
+    part.options.should have_key(:option_2)
+    part.options[:option_1].should eq('value 1')
+    part.options[:option_2].should eq('value 2')
   end
 end
